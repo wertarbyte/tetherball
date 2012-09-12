a1dbc4f6d67d0907b67f5e7ceaae26eaa26128d3
Stefan Tomanek <stefan@pico.ruhr.de>
2012-09-12 14:26:09 +0200

    move configuration items to the beginning

--- tetherball.sh
+++ tetherball.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+# tetherball.sh by Stefan Tomanek <stefan@pico.ruhr.de>
+WLAN_SSID="tetherball"
+WLAN_CHANNEL="1"
+WLAN_PSK="secretstuff"
+WLAN_OWN_ADDRESS="10.9.9.1/24"
+WLAN_DHCP_RANGE="10.9.9.100,10.9.9.150,255.255.255.0,1h"
+WLAN_DEV="wlan0"
+
+### no user servicable parts beyond this line ###
 IWCONFIG="iwconfig"
 IP="ip"
 HOSTAPD="hostapd"
@@ -6,19 +15,13 @@ DNSMASQ="dnsmasq"
 SYSCTL="sysctl"
 IPTABLES="iptables"
 
-WLAN_DEV="wlan0"
 AP_CONF="$(mktemp hostapd-${WLAN_DEV}-XXXXX --tmpdir --suffix=.conf)"
-
-WLAN_SSID="test"
-WLAN_PSK="secretstuff"
-WLAN_OWN_ADDRESS="10.9.9.1/24"
-WLAN_DHCP_RANGE="10.9.9.100-10.9.9.150,1h"
 
 cat <<EOF > "$AP_CONF"
 interface=$WLAN_DEV
 ssid=$WLAN_SSID
 hw_mode=g
-channel=1
+channel=$WLAN_CHANNEL
 wpa=2
 wpa_passphrase=$WLAN_PSK
 EOF
@@ -34,7 +37,7 @@ $IP link set $WLAN_DEV up
 $SYSCTL net.ipv4.ip_forward=1
 $IPTABLES -t nat -A POSTROUTING -s "$WLAN_OWN_ADDRESS" -j MASQUERADE
 
-$DNSMASQ -i wlan0 --dhcp-range 10.9.9.100,10.9.9.150,255.255.255.0,1h -d
+$DNSMASQ -i wlan0 --dhcp-range="$WLAN_DHCP_RANGE" -d
 
 # cleanup
 kill $HOSTAP_PID
